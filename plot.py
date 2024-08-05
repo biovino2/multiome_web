cd82288c0fe5f26270eac4d2ba91e8880b465585
biovino2 <benjamin.iovino@czbiohub.org>
2024-08-05 14:10:59 -0700

    understandable x-axis

--- plot.py
+++ plot.py
@@ -5,6 +5,7 @@ Ben Iovino  08/01/24    CZ-Biohub
 
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+from matplotlib.ticker import ScalarFormatter
 import numpy as np
 import os
 import pandas as pd
@@ -41,9 +42,16 @@ def compute_start_end(df: pd.DataFrame):
     
     """
 
-    df['chr'] = df['peak_id'].str.split('_').str[0].astype(str)
-    df['start'] = df['peak_id'].str.split('_').str[1].astype(int)
-    df['end'] = df['peak_id'].str.split('_').str[2].astype(int)
+    # Subset peak ID
+    peak_id = df['peak_id']
+    chrom = peak_id.str.split('_').str[0].astype(str)
+    start = peak_id.str.split('_').str[1].astype(int)/1000
+    end = peak_id.str.split('_').str[2].astype(int)/1000
+
+    # Assign to DF
+    df['chr'] = chrom
+    df['start'] = start
+    df['end'] = end
 
     return df
 
@@ -66,67 +74,42 @@ def plot_peaks(df: pd.DataFrame, ax: plt.Axes, y_base: int, color: str, label=No
         label = None  # Set to None to prevent repeating the label
 
 
-def plot_CCANs_genomic_loci(CCANs: 'list[pd.DataFrame]', timepoints: 'list[str]', gene_name: str, colordict: 'dict[str: np.array]', 
-                            save_fig=False, figpath = None):
-    """
-    This function takes a list of CCANs (dataframes for each timepoint), and plots the genomic
-      region with CCANs for each timepoint for "gene_name".
-    
-    Parameters:
-    1) CCANs (list[pd.Dataframe]): A list of dataframes (one dataframe for each timepoint), i.e. [df1, df2, df3]
-    2) timepoints (list(str)): A list of timepoint labels corresponding to each dataframe in CCANs
-    3) gene_name (str): Name of the gene, i.e. "myf5", "her1"
-    4) colordict (dict[str: np.array]): A dictionary of {timepoints:colors (viridis)}
-    """
-    
-    if len(CCANs) != len(timepoints):
-        raise ValueError("The number of CCANs dataframes and timepoints labels must be equal")
-    
-    # generate a figure object    
-    fig, ax = plt.subplots(figsize=(10, 2))
-    
-    genomic_start, genomic_end = float('inf'), 0
+def get_xticks(beg: int, end: int) -> 'list[int]':
+    """Returns xticks for the genomic region, depending on it's size.
 
-    for index, (df, stage) in enumerate(zip(CCANs, timepoints)):
-        # subset for the "gene_name" (gene of interest)
-        df = df[df.gene_short_name == gene_name]
-        df = compute_start_end(df)
-        
-        # # extract the chromosome and genomic coordinates
-        # if index==0:
-        #     chromosome = df.loc['chr'][0]
-        
-        genomic_start = min(genomic_start, df['start'].min())
-        genomic_end = max(genomic_end, df['end'].max())
+    Args:
+        beg (int): The beginning of the genomic region.
+        end (int): The end of the genomic region.
 
-        plot_peaks(df, ax, 1 + index*0.1, colordict[stage], stage)
-    
-    ax.plot([genomic_start, genomic_end], [1, 1], color='grey', linewidth=2)
-    
-    ax.set_ylim(0.7, 1 + len(CCANs)*0.1 + 0.5)
-    ax.set_yticks([])
-    ax.set_xlabel('Genomic Coordinate')
-    ax.set_title(f'Genomic Region Plot for {gene_name}')
-    ax.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
-    
-    plt.tight_layout()
-    
-    if save_fig==True:
-        plt.savefig(figpath + "coverage_plot_CCANs_" + gene_name + ".png")
-        plt.savefig(figpath + "coverage_plot_CCANs_" + gene_name + ".pdf")
-    
-    # Return plot
-    return fig
-
-
-def main():
-    """
+    Returns:
+        list[int]: The xticks for the genomic region plot.
     """
 
-    # Define the timepoints and load data in order
-    timepoints = {"0budstage": 'TDR126', "5somites": 'TDR127', "10somites": 'TDR128',
-                   "15somites": 'TDR118', "20somites": 'TDR125', "30somites": 'TDR124'}
-    df_list = load_dfs('data', list(timepoints.values()))
+    st.write(beg, end)
+    if end - beg <= 5.0:
+        mid = (beg + end) / 2
+        beg = mid-2.5
+        end = mid+2.5
+        st.write(beg, end, mid)
+        xticks = np.arange(beg, end, 1, dtype=int)
+    else:
+        diff = end - beg
+        xticks = np.arange(beg, end, diff/5)
+        xticks = np.append(xticks, end)
+
+
+    return xticks
+
+
+def get_color_dict(timepoints: 'dict[str, str]') -> 'dict[str, np.array]':
+    """Returns a dictionary mapping timepoints to colors.
+
+    Args:
+        timepoints (dict[str, str]): A dictionary mapping timepoints to labels.
+
+    Returns:
+        dict[str, np.array]: A dictionary mapping timepoints to colors.
+    """
 
     # Load the "viridis" colormap
     viridis = plt.cm.get_cmap('viridis', 256)
@@ -138,8 +121,78 @@ def main():
     colors = viridis(np.linspace(start/256, stop/256, len(timepoints)))
 
     # Create a dictionary to map timepoints to colors
-    color_dict = dict(zip(timepoints, colors))
+    color_dict = dict(zip(timepoints.keys(), colors))
+
+    return color_dict
+
+
+def plot_ccans_genomic_loci(dfs: 'list[pd.DataFrame]', timepoints: 'list[str]', gene_name: str,
+                            colordict: 'dict[str: np.array]', save_fig=False, figpath = None):
+    """
+    This function takes a list of CCANs (dataframes for each timepoint), and plots the genomic
+      region with CCANs for each timepoint for "gene_name".
     
+    Parameters:
+    1) dfs (list[pd.Dataframe]): A list of dataframes (one dataframe for each timepoint), i.e. [df1, df2, df3]
+    2) timepoints (list(str)): A list of timepoint labels corresponding to each dataframe in CCANs
+    3) gene_name (str): Name of the gene, i.e. "myf5", "her1"
+    4) colordict (dict[str: np.array]): A dictionary of {timepoints:colors (viridis)}
+    """
+
+    if len(dfs) != len(timepoints):
+        raise ValueError("The number of CCANs dataframes and timepoints labels must be equal")
+
+    # generate a figure object
+    fig, ax = plt.subplots(figsize=(10, 2))
+    genomic_start, genomic_end = float('inf'), 0
+
+     # one gene for each time point, extract genomic coordinates and plot peaks
+    for index, (df, stage) in enumerate(zip(dfs, timepoints)):
+        if df[df.gene_short_name == gene_name].empty:  # ignore if gene not present in timepoint
+            continue
+        df_gene = df[df.gene_short_name == gene_name]  # subset for the gene of interest
+        df_gene = compute_start_end(df_gene)
+
+        # update genomic start and end and plot peaks
+        genomic_start = min(genomic_start, df_gene['start'].min())
+        genomic_end = max(genomic_end, df_gene['end'].max())
+        plot_peaks(df_gene, ax, 1 + index*0.1, colordict[stage], stage)
+
+    ax.plot([genomic_start, genomic_end], [1, 1], color='grey', linewidth=2)
+
+    # Set and format xticks
+    ax.set_xticks(get_xticks(genomic_start, genomic_end))
+    formatter = ScalarFormatter(useOffset=False)
+    formatter.set_scientific(False)
+    ax.xaxis.set_major_formatter(formatter)
+
+    # Format rest of plot
+    ax.set_ylim(0.7, 1 + len(dfs)*0.1 + 0.5)
+    ax.set_yticks([])
+    ax.set_xlabel('Genomic Coordinate (kbp)')
+    ax.set_title(f'Genomic Region Plot for {gene_name} ({df_gene["chr"].iloc[0]})')
+    ax.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+
+    plt.tight_layout()
+
+    if save_fig:
+        plt.savefig(figpath + "coverage_plot_CCANs_" + gene_name + ".png")
+        plt.savefig(figpath + "coverage_plot_CCANs_" + gene_name + ".pdf")
+
+    # Return plot
+    return fig
+
+
+def main():
+    """
+    """
+
+    # Define the timepoints and load data in order
+    timepoints = {"0 budstage": 'TDR126', "5 somites": 'TDR127', "10 somites": 'TDR128',
+                   "15 somites": 'TDR118', "20 somites": 'TDR125', "30 somites": 'TDR124'}
+    df_list = load_dfs('data', list(timepoints.values()))
+    color_dict = get_color_dict(timepoints)
+
     # Get all gene names from the data and have user choose
     gene_names = df_list[0]['gene_short_name'].unique()
     option = st.selectbox(
@@ -148,7 +201,7 @@ def main():
     )
 
     # Create and plot figure
-    fig = plot_CCANs_genomic_loci(df_list,
+    fig = plot_ccans_genomic_loci(df_list,
                                 gene_name=option,
                                 timepoints=list(timepoints.keys()),
                                 colordict=color_dict)
