5c373652d30eeb24a6f4d8444172a6ce47bfe8f3
biovino2 <benjamin.iovino@czbiohub.org>
2024-09-24 13:36:18 -0700

    isolate cell types

--- corr/plot_corr.py
+++ corr/plot_corr.py
@@ -15,17 +15,17 @@ import sys
 from preprocess import define_color_dict
 
 
-def st_setup(gene_names: 'list[str]'):
+def st_setup() -> str:
     """Initializes streamlit session.
 
-    Args:
-        gene_names: The list of gene names.
+    Returns:
+        str: The selected cell type.
     """
 
     st.set_page_config(layout="wide")
     st.sidebar.markdown('# Settings')
     st.title('ATAC and RNA Correlation')
-    st.write('For each time point, we plot plot time-resolved scatter plots of ATAC (x-axis) and RNA expression (y-axis) ' \
+    st.write('For each time point, we plot plot time-resolved scatter plots of chromatin accessibility (x-axis) and gene expression (y-axis) ' \
              'for any gene. Each point represents a metacell (SEACell), colored by cell type. The correlation between ' \
               'chromatin accessbility and gene expression is also calculated for each time point.')
     
@@ -56,19 +56,16 @@ def st_setup(gene_names: 'list[str]'):
             if st.button('Reset'):
                 st.session_state.selectboxes = [0]
 
-    # Create selectboxes
-    selected_genes = []
-    default = gene_names.index('slc4a1a')
-    for key in st.session_state.selectboxes:
-        st.sidebar.selectbox(
-            'Select a gene to plot',
-            gene_names,
-            key=key,
-            index=default
-        )
-        selected_genes.append(st.session_state[key])
+    # Create selectbox for cell type selection
+    celltypes = list(define_color_dict().keys())
+    celltypes.insert(0, 'All')
+    celltype = st.sidebar.selectbox(
+        'Isolate a cell type',
+        celltypes,
+        index=celltypes.index('All')
+    )
 
-    return selected_genes
+    return celltype
 
 
 def save_config() -> dict:
@@ -92,10 +89,93 @@ def save_config() -> dict:
     return config
 
 
-def plot_genes(gene: str, gene_dict: 'dict[str:sc.AnnData]') -> plt.Figure:
+def subset_data(gene_dict: dict, gene: str, sample_id: str, celltype: str) -> 'tuple[np.ndarray, np.ndarray, list[str], list[str]]':
+    """Returns gene expression and activity values as arrays, as well as colors for each cell type.
+
+    Args:
+        gene_dict (dict): The dictionary of genes and their values for each time point.
+        gene (str): The gene to plot.
+        sample_id (str): The timepoint.
+        celltype (str): The selected cell type.
+
+    Returns:
+        tuple (np.ndarray, np.ndarray, list[str], list[str]): The gene expression values, activity values, colors, and cell type.
+    """
+
+    # Get data for timepoint
+    expr_rna = gene_dict[gene][sample_id][0]
+    expr_atac = gene_dict[gene][sample_id][1]
+    colors = gene_dict[gene][sample_id][2]
+
+    # Map colors to cell types
+    color_dict = define_color_dict()
+    inv_color_dict = {v: k for k, v in color_dict.items()}
+    cell_colors = [inv_color_dict[color] for color in colors]
+
+    # Isolate points related to celltype
+    if celltype != 'All':
+        indices = [i for i, cell in enumerate(cell_colors) if cell == celltype]
+        expr_rna = expr_rna[indices]
+        expr_atac = expr_atac[indices]
+        colors = [colors[i] for i in indices]
+        cell_colors = [cell_colors[i] for i in indices]
+
+    return expr_rna, expr_atac, colors, cell_colors
+
+
+def calc_corr(expr_rna: np.ndarray, expr_atac: np.ndarray) -> float:
+    """Returns the correlation between two arrays.
+
+    Args:
+        expr_rna (np.ndarray): RNA expression values.
+        expr_atac (np.ndarray): ATAC expression values.
+
+    Returns:
+        float: The correlation value.
+    """
+
+    if len(expr_rna) == 0 or len(expr_atac) == 0:
+        return np.nan
+
+    # Calculate correlation
+    if np.all(expr_rna == expr_rna[0]) or np.all(expr_atac== expr_atac[0]):
+        return np.nan
+    else:
+        correlation, _ = pearsonr(expr_rna, expr_atac)
+        return correlation
+    
+
+def get_margins(expr_rna: np.ndarray, expr_atac: np.ndarray, rna_max: float, atac_max: float) -> 'tuple[float, float]':
+    """Returns maximum values for RNA and ATAC expression.
+
+    Args:
+        expr_rna (np.ndarray): RNA expression values.
+        expr_atac (np.ndarray): ATAC expression values.
+        rna_max (float): The current maximum RNA value.
+        atac_max (float): The current maximum ATAC value.
+
+    Returns:
+        tuple (float, float): The updated maximum values.
+    """
+
+    # No values if no cells are present at timepoint
+    if len(expr_rna) == 0 or len(expr_atac) == 0:
+        return rna_max, atac_max
+    
+    # Update margins of the plot
+    if np.max(expr_rna) > rna_max:
+        rna_max = np.max(expr_rna)
+    if np.max(expr_atac) > atac_max:
+        atac_max = np.max(expr_atac)
+
+    return rna_max, atac_max
+
+
+def plot_genes(celltype: str, gene: str, gene_dict: 'dict[str:sc.AnnData]') -> plt.Figure:
     """Returns one figure containing scatter plots for each gene.
 
     Args:
+        celltype (str): The selected cell type.
         genes (str): Gene to plot
         dict_meta (dict[str:sc.AnnData]): The dictionary of adata objects.
 
@@ -116,24 +196,8 @@ def plot_genes(gene: str, gene_dict: 'dict[str:sc.AnnData]') -> plt.Figure:
     rna_max, atac_max = 0, 0
     for index, sample_id in enumerate(list(timepoints.keys())):
 
-        # Get data for timepoint
-        expr_rna = gene_dict[gene][sample_id][0]
-        expr_atac = gene_dict[gene][sample_id][1]
-        colors = gene_dict[gene][sample_id][2]
-
-        # Calculate correlation
-        if np.all(expr_rna == expr_rna[0]) or np.all(expr_atac== expr_atac[0]):
-            corr_scores.append(np.nan)
-        else:
-            correlation, _ = pearsonr(expr_rna, expr_atac)
-            corr_scores.append(correlation)
-
-        # Map colors to cell types
-        color_dict = define_color_dict()
-        inv_color_dict = {v: k for k, v in color_dict.items()}
-        type_colors = [inv_color_dict[color] for color in colors]
-
         # Plot scatter plot with plotly
+        expr_rna, expr_atac, colors, type_colors = subset_data(gene_dict, gene, sample_id, celltype)
         scatter = go.Scatter(
                         x=expr_atac,
                         y=expr_rna,
@@ -143,11 +207,9 @@ def plot_genes(gene: str, gene_dict: 'dict[str:sc.AnnData]') -> plt.Figure:
                         text=type_colors,
                     )
         
-        # Update margins of the plot
-        if np.max(expr_rna) > rna_max:
-            rna_max = np.max(expr_rna)
-        if np.max(expr_atac) > atac_max:
-            atac_max = np.max(expr_atac)
+        # Calculate correlation and update margins with max rna/atac values
+        corr_scores.append(calc_corr(expr_rna, expr_atac))
+        rna_max, atac_max = get_margins(expr_rna, expr_atac, rna_max, atac_max)
 
         # Add go.Scatter object to figure
         fig.add_trace(scatter, row=(index//3)+1, col=(index%3)+1)
@@ -167,7 +229,7 @@ def plot_genes(gene: str, gene_dict: 'dict[str:sc.AnnData]') -> plt.Figure:
     # Update axes labels and titles
     for i in range (0, len(corr_scores)):
         fig.update_xaxes(title='ATAC', row=i//3+1, col=i%3+1, title_font=dict(size=10), range=[0, atac_max], title_standoff=4)
-        fig.update_yaxes(title='RNA', row=i//3+1, col=i%3+1, title_font=dict(size=10), range=[0, rna_max], title_standoff=5)
+        fig.update_yaxes(title='RNA', row=i//3+1, col=i%3+1, title_font=dict(size=10), range=[0, rna_max], title_standoff=8)
 
     return fig
 
@@ -183,11 +245,23 @@ def main():
     # Load data and set up streamlit
     gene_dict = pkl.load(open(f"{path}/gene_dict.pkl", "rb"))
     gene_names = list(gene_dict.keys())
-    selected_genes = st_setup(gene_names)
+    celltype = st_setup()
+
+    # Create selectboxes
+    selected_genes = []
+    default = gene_names.index('slc4a1a')
+    for key in st.session_state.selectboxes:
+        st.sidebar.selectbox(
+            'Select a gene to plot',
+            gene_names,
+            key=key,
+            index=default
+        )
+        selected_genes.append(st.session_state[key])
 
     # Plot each figure
     for gene in selected_genes:
-        fig = plot_genes(gene, gene_dict)
+        fig = plot_genes(celltype, gene, gene_dict)
         st.markdown(f'### {gene}')
         st.plotly_chart(fig, config=save_config())
 
